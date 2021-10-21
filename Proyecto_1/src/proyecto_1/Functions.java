16ce2709676d3a182166f5e609f9d48fa5364e4b
AlexSnowy <80966206+AlexSnowy@users.noreply.github.com>
2021-10-21 13:50:19 -0400

    Equivalence Function and Proyect Simulation

--- Proyecto_1/src/proyecto_1/Functions.java
+++ Proyecto_1/src/proyecto_1/Functions.java
@@ -6,61 +6,61 @@ public class Functions {
         
     }
     
-    public void changeRoutesEntrance(RouteList routes){
+    public void changeRoutesEntrance(RouteList routes, ClientsList clients){
         RouteNode aux = routes.getFirst();
         for (int i = 0; i < routes.getSize(); i++){
             if (aux.getEntrance() == "A"){
-                aux.setEntrance(7);
+                aux.setEntrance(clients.getSize());
             }else if (aux.getEntrance() == "B"){
-                aux.setEntrance(8);
+                aux.setEntrance(clients.getSize() + 1);
             }else if (aux.getEntrance() == "C"){
-                aux.setEntrance(9);
+                aux.setEntrance(clients.getSize() + 2);
             }else if (aux.getEntrance() == "D"){
-                aux.setEntrance(10);
+                aux.setEntrance(clients.getSize() + 3);
             }else if (aux.getEntrance() == "E"){
-                aux.setEntrance(11);
+                aux.setEntrance(clients.getSize() + 4);
             }else if (aux.getEntrance() == "F"){
-                aux.setEntrance(12);
+                aux.setEntrance(clients.getSize() + 5);
             }else if (aux.getEntrance() == "G"){
-                aux.setEntrance(13);
+                aux.setEntrance(clients.getSize() + 6);
             }else if (aux.getEntrance() == "H"){
-                aux.setEntrance(14);
+                aux.setEntrance(clients.getSize() + 7);
             }else if (aux.getEntrance() == "I"){
-                aux.setEntrance(15);
+                aux.setEntrance(clients.getSize() + 8);
             }else if (aux.getEntrance() == "J"){
-                aux.setEntrance(16);
+                aux.setEntrance(clients.getSize() + 9);
             }else if (aux.getEntrance() == "K"){
-                aux.setEntrance(17);
+                aux.setEntrance(clients.getSize() + 10);
             }else if (aux.getEntrance() == "L"){
-                aux.setEntrance(18);
+                aux.setEntrance(clients.getSize() + 11);
             }else if (aux.getEntrance() == "M"){
-                aux.setEntrance(19);
+                aux.setEntrance(clients.getSize() + 12);
             }else if (aux.getEntrance() == "N"){
-                aux.setEntrance(20);
+                aux.setEntrance(clients.getSize() + 13);
             }else if (aux.getEntrance() == "O"){
-                aux.setEntrance(21);
+                aux.setEntrance(clients.getSize() + 14);
             }else if (aux.getEntrance() == "P"){
-                aux.setEntrance(22);
+                aux.setEntrance(clients.getSize() + 15);
             }else if (aux.getEntrance() == "Q"){
-                aux.setEntrance(23);
+                aux.setEntrance(clients.getSize() + 16);
             }else if (aux.getEntrance() == "R"){
-                aux.setEntrance(24);
+                aux.setEntrance(clients.getSize() + 17);
             }else if (aux.getEntrance() == "S"){
-                aux.setEntrance(25);
+                aux.setEntrance(clients.getSize() + 18);
             }else if (aux.getEntrance() == "T"){
-                aux.setEntrance(26);
+                aux.setEntrance(clients.getSize() + 19);
             }else if (aux.getEntrance() == "U"){
-                aux.setEntrance(27);
+                aux.setEntrance(clients.getSize() + 20);
             }else if (aux.getEntrance() == "V"){
-                aux.setEntrance(28);
+                aux.setEntrance(clients.getSize() + 21);
             }else if (aux.getEntrance() == "W"){
-                aux.setEntrance(29);
+                aux.setEntrance(clients.getSize() + 22);
             }else if (aux.getEntrance() == "X"){
-                aux.setEntrance(30);
+                aux.setEntrance(clients.getSize() + 23);
             }else if (aux.getEntrance() == "Y"){
-                aux.setEntrance(31);
+                aux.setEntrance(clients.getSize() + 24);
             }else if (aux.getEntrance() == "Z"){
-                aux.setEntrance(32);
+                aux.setEntrance(clients.getSize() + 25);
             }else if (aux.getEntrance() == "1"){
                 aux.setEntrance(0);
             }else if (aux.getEntrance() == "2"){
@@ -75,66 +75,92 @@ public class Functions {
                 aux.setEntrance(5);
             }else if (aux.getEntrance() == "7"){
                 aux.setEntrance(6);
+            }else if (aux.getEntrance() == "8"){
+                aux.setEntrance(7);
+            }else if (aux.getEntrance() == "9"){
+                aux.setEntrance(8);
+            }else if (aux.getEntrance() == "10"){
+                aux.setEntrance(9);
+            }else if (aux.getEntrance() == "11"){
+                aux.setEntrance(10);
+            }else if (aux.getEntrance() == "12"){
+                aux.setEntrance(11);
+            }else if (aux.getEntrance() == "13"){
+                aux.setEntrance(12);
+            }else if (aux.getEntrance() == "14"){
+                aux.setEntrance(13);
+            }else if (aux.getEntrance() == "15"){
+                aux.setEntrance(14);
+            }else if (aux.getEntrance() == "16"){
+                aux.setEntrance(15);
+            }else if (aux.getEntrance() == "17"){
+                aux.setEntrance(16);
+            }else if (aux.getEntrance() == "18"){
+                aux.setEntrance(17);
+            }else if (aux.getEntrance() == "19"){
+                aux.setEntrance(18);
+            }else if (aux.getEntrance() == "20"){
+                aux.setEntrance(19);
             }
             aux = aux.getNext();
         }
     }
     
-    public void changeRoutesExits(RouteList routes){
+    public void changeRoutesExits(RouteList routes, ClientsList clients){
         RouteNode aux = routes.getFirst();
         for (int i = 0; i < routes.getSize(); i++){
             if (aux.getExit()== "A"){
-                aux.setExit(7);
+                aux.setExit(clients.getSize());
             }else if (aux.getExit() == "B"){
-                aux.setExit(8);
+                aux.setExit(clients.getSize() + 1);
             }else if (aux.getExit() == "C"){
-                aux.setExit(9);
+                aux.setExit(clients.getSize() + 2);
             }else if (aux.getExit() == "D"){
-                aux.setExit(10);
+                aux.setExit(clients.getSize() + 3);
             }else if (aux.getExit() == "E"){
-                aux.setExit(11);
+                aux.setExit(clients.getSize() + 4);
             }else if (aux.getExit() == "F"){
-                aux.setExit(12);
+                aux.setExit(clients.getSize() + 5);
             }else if (aux.getExit() == "G"){
-                aux.setExit(13);
+                aux.setExit(clients.getSize() + 6);
             }else if (aux.getExit() == "H"){
-                aux.setExit(14);
+                aux.setExit(clients.getSize() + 7);
             }else if (aux.getExit() == "I"){
-                aux.setExit(15);
+                aux.setExit(clients.getSize() + 8);
             }else if (aux.getExit() == "J"){
-                aux.setExit(16);
+                aux.setExit(clients.getSize() + 9);
             }else if (aux.getExit() == "K"){
-                aux.setExit(17);
+                aux.setExit(clients.getSize() + 10);
             }else if (aux.getExit() == "L"){
-                aux.setExit(18);
+                aux.setExit(clients.getSize() + 11);
             }else if (aux.getExit() == "M"){
-                aux.setExit(19);
+                aux.setExit(clients.getSize() + 12);
             }else if (aux.getExit() == "N"){
-                aux.setExit(20);
+                aux.setExit(clients.getSize() + 13);
             }else if (aux.getExit() == "O"){
-                aux.setExit(21);
+                aux.setExit(clients.getSize() + 14);
             }else if (aux.getExit() == "P"){
-                aux.setExit(22);
+                aux.setExit(clients.getSize() + 15);
             }else if (aux.getExit() == "Q"){
-                aux.setExit(23);
+                aux.setExit(clients.getSize() + 16);
             }else if (aux.getExit() == "R"){
-                aux.setExit(24);
+                aux.setExit(clients.getSize() + 17);
             }else if (aux.getExit() == "S"){
-                aux.setExit(25);
+                aux.setExit(clients.getSize() + 18);
             }else if (aux.getExit() == "T"){
-                aux.setExit(26);
+                aux.setExit(clients.getSize() + 19);
             }else if (aux.getExit() == "U"){
-                aux.setExit(27);
+                aux.setExit(clients.getSize() + 20);
             }else if (aux.getExit() == "V"){
-                aux.setExit(28);
+                aux.setExit(clients.getSize() + 21);
             }else if (aux.getExit() == "W"){
-                aux.setExit(29);
+                aux.setExit(clients.getSize() + 22);
             }else if (aux.getExit() == "X"){
-                aux.setExit(30);
+                aux.setExit(clients.getSize() + 23);
             }else if (aux.getExit() == "Y"){
-                aux.setExit(31);
+                aux.setExit(clients.getSize() + 24);
             }else if (aux.getExit() == "Z"){
-                aux.setExit(32);
+                aux.setExit(clients.getSize() + 25);
             }else if (aux.getExit() == "1"){
                 aux.setExit(0);
             }else if (aux.getExit() == "2"){
@@ -149,6 +175,32 @@ public class Functions {
                 aux.setExit(5);
             }else if (aux.getExit() == "7"){
                 aux.setExit(6);
+            }else if (aux.getExit() == "8"){
+                aux.setExit(7);
+            }else if (aux.getExit() == "9"){
+                aux.setExit(8);
+            }else if (aux.getExit() == "10"){
+                aux.setExit(9);
+            }else if (aux.getExit() == "11"){
+                aux.setExit(10);
+            }else if (aux.getExit() == "12"){
+                aux.setExit(11);
+            }else if (aux.getExit() == "13"){
+                aux.setExit(12);
+            }else if (aux.getExit() == "14"){
+                aux.setExit(13);
+            }else if (aux.getExit() == "15"){
+                aux.setExit(14);
+            }else if (aux.getExit() == "16"){
+                aux.setExit(15);
+            }else if (aux.getExit() == "17"){
+                aux.setExit(16);
+            }else if (aux.getExit() == "18"){
+                aux.setExit(17);
+            }else if (aux.getExit() == "19"){
+                aux.setExit(18);
+            }else if (aux.getExit() == "20"){
+                aux.setExit(19);
             }
             aux = aux.getNext();
         }
